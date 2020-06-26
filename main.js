e605c4977deb7ce49b5d739373dd1a36ebfd4e73
Leia <leiahopf@googlemail.com>
2020-06-26 10:33:02 +0100

    Fix: Name-change; page arrangements; add Comments;

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
+//Constants and Variables
+    //SVG Setup
 const draw = SVG().addTo('.canvas').size(400, 400);
 const playground = draw.rect(400, 400).fill({color:'#ccc', opacity:0.75}).stroke({color:'#888', width:6});
-
-const displayPopulation = document.querySelector('.info span');
+    
+    //Getting Elements from DOM
+const populationElement = document.querySelector('.info span');
 const birthElement = document.querySelector('.births');
 const deathElement = document.querySelector('.deaths');
 
+    //Setting Variables Needed
+let birthrate = 0;
+let deathrate = 0;
+let growthrate = birthrate - deathrate;
+let population = [];
+let newName = 0;
 
+//Getting Input from User & Setting Rates to User's Input
 birthElement.addEventListener('keypress', setRates);
 deathElement.addEventListener('keypress', setRates);
 
@@ -13,27 +23,18 @@ function setRates(event){
     if(event.keyCode == 13){
         birthrate = birthElement.value;
         deathrate = deathElement.value;
-        startRound();
+        startSimulation();
     };
 }
 
-let birthrate = 0;
-let deathrate = 0;
-let growthrate = birthrate - deathrate;
-//Population Size (P) (get from code)
-//Birth Rate (B)(The number of live births per 1,000 population in a given year.)
-// B = b(set by user)/P(get from code)
-//Death Rate (D)(The number of live deaths per 1,000 population in a given year. )
-// D = d(set by user)/P(get from code)
-//birth and death rates for humans can also be viewed as the probability that any one person will give birth or die in a year.
-// r = b - d (growth rate)
-
+//Produce Random Integers between some Min and some Max
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 };
 
+//Creature/Life - Object Constructor
 class Life{
     constructor(name, xcoord, ycoord, bodysize){
     this.name = name;
@@ -44,13 +45,16 @@ class Life{
     moveAbout(movement, wait){
         for (let i=0; i < movement; i++){
         this.body.animate().delay(wait).dx(getRandomInt(-40, 41)).dy(getRandomInt(-40,41))
-    }}
-    
+    }}   
 }
 
-let population = [];
-let newName = 0;
+//Start Simulation
+function startSimulation(){
+    setInterval(suddenBirth, 1000, birthrate);
+    setInterval(suddenDeath, 1000, deathrate);
+    }
 
+    //Birth Chance per Interval
 function suddenBirth(birthrate){
     let chance = getRandomInt(0, 100);
     if (chance < birthrate){
@@ -58,11 +62,10 @@ function suddenBirth(birthrate){
         newLife.moveAbout(100, getRandomInt(1, 300));
         population.push(newLife);
     }
-    displayPopulation.innerHTML = '   ' + population.length;
-    console.log(`Population Size: ${population.length}`);
+    populationElement.innerHTML = '   ' + population.length;
 }
 
-
+    //Death Chance per Interval
 function suddenDeath(deathrate){
     let chance = getRandomInt(0, 100);
     if (chance < deathrate){
@@ -72,9 +75,5 @@ function suddenDeath(deathrate){
        console.log(population[0]);
        population.shift(population[0]);
     }
-}
-
-function startRound(){
-setInterval(suddenBirth, 1000, birthrate);
-setInterval(suddenDeath, 1000, deathrate);
+    populationElement.innerHTML = '   ' + population.length;
 }
